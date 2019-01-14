6b291f7bf97f15a56893440923a02be7e7eca8eb
wangkofend@163.com <1q2w3e5T>
2019-01-14 11:53:27 +0800

    第一次

---
+++ web/src/main/java/com/wjh/job/web/ctrl/JobInfoCtrl.java
@@ -0,0 +1,15 @@
+package com.wjh.job.web.ctrl;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class JobInfoCtrl {
+
+    @RequestMapping(value="/jobinfo/detail",method = RequestMethod.POST,produces = "application/json")
+    @ResponseBody
+    String getInfo(@RequestBody JSONObject jsonParam) {
+        return jsonParam.toJSONString();
+    }
+}
