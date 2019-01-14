25e97f1af56c5d7e743ec5e3f72077e5adc05530
wangkofend@163.com <1q2w3e5T>
2019-01-14 22:18:50 +0800

    第一次

--- web/src/main/java/com/wjh/job/web/ctrl/TestCtrl.java
+++ web/src/main/java/com/wjh/job/web/ctrl/TestCtrl.java
@@ -3,12 +3,15 @@ package com.wjh.job.web.ctrl;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class TestCtrl {
 
-    @RequestMapping(value="/test",method = RequestMethod.GET,produces = "application/json")
+    @RequestMapping(value="/test",method = RequestMethod.GET)
     @ResponseBody
-    String getInfo(@RequestBody JSONObject jsonParam) {
+    String getInfo(HttpServletRequest request) {
         return "OK";
     }
 }
